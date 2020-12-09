1df86f3a05fdcf0e1ee9836a3675381fddf6ec92
Sapphon <sapphon@gmail.com>
2020-12-09 05:34:41 -0500

    Begin to add build mode area picker [https://trello.com/c/PMBeZIJg]

---
+++ Assets/AreaPartPicker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AreaPartPicker : MonoBehaviour
+{
+
+    public Sprite buttonBackground;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] areaPrefabs = FindObjectOfType<BuildModeLevelModel>().areaParts;
+        for (int i = 0; i < areaPrefabs.Length; i++)
+        {
+            GameObject buttonObject = DefaultControls.CreateButton(new DefaultControls.Resources());
+            buttonObject.transform.SetParent(this.gameObject.transform);
+            Text buttonText = buttonObject.GetComponentInChildren<Text>();
+            buttonText.text = areaPrefabs[i].name;
+            Image buttonImage = buttonObject.GetComponent<Image>();
+            buttonImage.sprite = buttonBackground;
+        }
+    }
+
+// Update is called once per frame
+    void Update()
+    {
+    }
+}
