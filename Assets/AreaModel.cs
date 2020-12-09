ac64bcbedb6a9d7ce3307b4587cd61213026e59b
Sapphon <sapphon@gmail.com>
2020-12-09 04:31:48 -0500

    Add remaining prefabs needed for demo #1 and area model [https://trello.com/c/PMBeZIJg]
    Area model currently assumes rectangular: height/width with offsets for exits (which are assumed to be 1m wide always)

---
+++ Assets/AreaModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaModel : MonoBehaviour{
+
+public int length;
+public int height;
+public int[] exitNorthOffsets;
+public int[] exitEastOffsets;
+public int[] exitSouthOffsets;
+public int[] exitWestOffsets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
