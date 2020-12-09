7c030e87c671e6735f19d4f7c9f7a5a7f9d8c429
Sapphon <sapphon@gmail.com>
2020-12-09 01:46:44 -0500

    Initial code commit with camera controller [https://trello.com/c/mSeiPBVj]

---
+++ Assets/KeyboardIsometricCameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardIsometricCameraController : MonoBehaviour
+{
+    private float speed = .03f;
+    
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(speed, 0, speed, Space.World);
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(-speed, 0, -speed, Space.World);
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(-speed, 0, speed, Space.World);
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(speed, 0, -speed, Space.World);
+        }
+    }
+}
