f6a8c69e8db192456b027a7f68af111d5d131fc0
Sapphon <sapphon@gmail.com>
2020-12-10 04:09:07 -0500

    AreaPartPicker Buttons now show outline of part selected on cursor [https://trello.com/c/3KWM7kcS]

--- Assets/AreaPartPicker.cs
+++ Assets/AreaPartPicker.cs
@@ -1,30 +1,106 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class AreaPartPicker : MonoBehaviour
 {
-
+    
     public GameObject paneTemplate;
+    public Material validMaterial;
+    private GameObject _selectedArea;
+    private Plane _areaPartSurface;
+    private GameObject _areaPreview;
+
     void Start()
     {
         GameObject[] areaPrefabs = FindObjectOfType<BuildModeLevelModel>().areaParts;
         float buttonHeight = paneTemplate.GetComponent<RectTransform>().rect.height;
-        
+        this._selectedArea = null;
+        this._areaPartSurface = new Plane(new Vector3(0, 1, 0), 0);
+        this._areaPreview = new GameObject();
         AddButtonsToPanel(areaPrefabs, buttonHeight);
         ResizePanelToFitButtons(areaPrefabs, buttonHeight);
     }
 
+    void Update()
+    {
+        if (_selectedArea != null)
+        {
+            ShowSelectedAreaAtCursor();
+        }
+        else
+        {
+            DisablePreview();
+        }
+    }
+
+    private void ShowSelectedAreaAtCursor()
+    {
+        SetPreviewMeshes();
+        SetPreviewLocation(getCursorPositionOnBuildingPlane());
+        SetPreviewMaterial(validMaterial);
+    }
+
+    private void SetPreviewMeshes()
+    {
+        Destroy(this._areaPreview);
+        this._areaPreview = Instantiate(_selectedArea);
+    }
+
+    private void DisablePreview()
+    {
+        SetPreviewMaterial(null);
+    }
+
+    private void SetPreviewMaterial(Material toSet)
+    {
+        foreach (MeshRenderer renderer in this._areaPreview.GetComponentsInChildren<MeshRenderer>())
+        {
+            renderer.material = toSet;
+        }
+    }
+
+    private void SetPreviewLocation(Vector3 vector3)
+    {
+        this._areaPreview.transform.position = getCursorPositionOnBuildingPlane() + new Vector3(0,.5f,0);
+    }
+
+    private Vector3 getCursorPositionOnBuildingPlane()
+    {
+        float distance;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (_areaPartSurface.Raycast(ray, out distance))
+        {
+            return ray.GetPoint(distance);
+        }
+        throw new Exception("Cannot find building surface");
+    }
+
     private void AddButtonsToPanel(GameObject[] areaPrefabs, float buttonHeight)
     {
+        void buttonClicked(int index)
+        {
+            this._selectedArea = areaPrefabs[index];
+        }
         for (int i = 0; i < areaPrefabs.Length; i++)
         {
             var paneObject = CreateButton();
             PositionButtonVertically(-i * (buttonHeight + 5f), paneObject);
-            SetButtonText(areaPrefabs[i].name, paneObject);
+            Button button = paneObject.GetComponentInChildren<Button>();
+            SetButtonText(areaPrefabs[i].name, button.gameObject);
+            int youHaveToDoThisInCSharpItsSilly = i;
+            SetButtonCallback(() => buttonClicked(youHaveToDoThisInCSharpItsSilly), button);
         }
+    }
+
+    private void SetButtonCallback(UnityAction action, Button button)
+    {
+        button.onClick.AddListener(action);
     }
 
     private static void PositionButtonVertically(float offset, GameObject paneObject)
@@ -37,9 +113,8 @@ public class AreaPartPicker : MonoBehaviour
         return Instantiate(paneTemplate, this.gameObject.transform);
     }
 
-    private static void SetButtonText(String text, GameObject paneObject)
+    private static void SetButtonText(String text, GameObject buttonObject)
     {
-        GameObject buttonObject = paneObject.GetComponentInChildren<Button>().gameObject;
         Text buttonText = buttonObject.GetComponentInChildren<Text>();
         buttonText.text = text;
     }
