359f538a14714c252034d3f00a7223524838f301
Sapphon <sapphon@gmail.com>
2020-12-10 22:22:26 -0500

    AreaPartController to handle material swaps [https://trello.com/c/kca8Yo3v]
    Not sure yet that those actually need to be object-oriented though

---
+++ Assets/Scripts/Behavior/AreaPartController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaPartController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void SetPartMaterial(Material toSet)
+    {
+
+        foreach (MeshRenderer renderer in this.gameObject.GetComponentsInChildren<MeshRenderer>())
+        {
+            if (toSet == null)
+            {
+                renderer.enabled = false;
+            }else{ 
+                renderer.enabled = true;
+                renderer.material = toSet;
+            }
+        }
+    }
+}
