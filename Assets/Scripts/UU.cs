b289c4af4470004310eee2f025a18b0e27a0f912
Sapphon <sapphon@gmail.com>
2020-12-10 22:20:14 -0500

    Create static utilities file [https://trello.com/c/kca8Yo3v]

---
+++ Assets/Scripts/UU.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UU 
+{
+    public static T GetOrAddComponent<T>(UnityEngine.GameObject gO) where T : UnityEngine.Component
+    {
+        T componentMaybe = gO.GetComponent<T>();
+        if (componentMaybe != null)
+        {
+            return componentMaybe;
+        }
+        return gO.AddComponent<T>();
+    }
+}
